8db40a8e045b6f84d6feab57905eb4dea5f43ffb
Gergő Szabó <46706621+gergoe-szabo@users.noreply.github.com>
2022-06-10 00:30:19 +0200

    changed epochs and batch sizes

--- Finetune/FT_T5_SUM.py
+++ Finetune/FT_T5_SUM.py
@@ -65,11 +65,11 @@ training_args = Seq2SeqTrainingArguments(
     output_dir="./results",
     evaluation_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    per_device_train_batch_size=12,
+    per_device_eval_batch_size=12,
     weight_decay=0.01,
     save_total_limit=3,
-    num_train_epochs=1,
+    num_train_epochs=8,
     fp16=True,
 )
 
