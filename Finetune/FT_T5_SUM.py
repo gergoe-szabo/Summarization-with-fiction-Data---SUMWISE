5169d89bb2754a843e80e800f21a6a9a4fdd4478
Gergő Szabó <46706621+gergoe-szabo@users.noreply.github.com>
2022-06-15 16:02:59 +0200

    Update FT_T5_SUM.py

--- Finetune/FT_T5_SUM.py
+++ Finetune/FT_T5_SUM.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 # coding: utf-8
 
 # In[ ]:
+
 
 from datasets import load_dataset, Dataset
 import pandas as pd
@@ -12,15 +13,30 @@ from transformers import AutoTokenizer
 # In[ ]:
 
 
-with open('booksum_train.json', "r") as f:
-    dataset = Dataset.from_dict(json.load(f))
+with open('/home/g-ball/Desktop/Projects/THESIS/Booksum/Dataset/booksum_train.json', "r", encoding="utf-8") as f:
+    train_data = Dataset.from_dict(json.load(f))
+
 
 # In[ ]:
-    
-dataset = dataset.train_test_split(test_size=0.2)
 
-tokenizer = AutoTokenizer.from_pretrained("google/t5-small")
+
+with open('/home/g-ball/Desktop/Projects/THESIS/Booksum/Dataset/booksum_val.json', "r", encoding="utf-8") as f:
+    val_data = Dataset.from_dict(json.load(f))
+
+
 # In[ ]:
+
+
+len_train = len(train_data)
+print("entities in training dataset: ", len_train)
+len_val = len(val_data)
+print("entities in evaluation dataset: ", len_val)
+
+
+# In[ ]:
+
+
+tokenizer = AutoTokenizer.from_pretrained("t5-small")
 
 prefix = "summarize: "
 
@@ -35,26 +51,18 @@ def preprocess_function(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
+tokenized_train_data = train_data.map(preprocess_function, batched=True)
 
-# In[ ]:
-
-
-tokenized_dataset = dataset.map(preprocess_function, batched=True)
-
-
-
-# In[ ]:
-
-
-from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
-
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
+tokenized_val_data = val_data.map(preprocess_function, batched=True)
 
 
 # In[ ]:
 
 
 from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
 
 data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
@@ -63,28 +71,41 @@ data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./results-T5-finetuned",
+
+    output_dir="./results-FN-T5-Small-Booksum",
+
     evaluation_strategy="epoch",
+
     learning_rate=2e-5,
+
     per_device_train_batch_size=12,
+
     per_device_eval_batch_size=12,
+
     weight_decay=0.01,
-    save_total_limit=3,
-    num_train_epochs=8,
+
+    save_total_limit=5,
+
+    num_train_epochs=1,
+
     fp16=True,
+
 )
 
 trainer = Seq2SeqTrainer(
+
     model=model,
+
     args=training_args,
-    train_dataset=tokenized_dataset["train"],
-    eval_dataset=tokenized_dataset["validation"],
+
+    train_dataset=tokenized_train_data,
+
+    eval_dataset=tokenized_val_data,
+
     tokenizer=tokenizer,
+
     data_collator=data_collator,
+
 )
 
 trainer.train()
-
-
-
-
